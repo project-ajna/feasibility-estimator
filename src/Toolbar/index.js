abb4eb60458bbcc5e6ef1472531362767f6276c4
Jikku Jose <jikkujose@gmail.com>
2018-07-25 17:28:55 +0530

    Update Toolbar

--- src/Toolbar/index.js
+++ src/Toolbar/index.js
@@ -2,9 +2,14 @@ import React from "react"
 
 export function Toolbar() {
   return (
-    <div className="py-4 px-5 bg-grey-lightest flex shadow">
-      <div className="p-3 bg-grey-light rounded hover:bg-grey">
-        Ajna | Feasibility Estimator
+    <div>
+      <div className="py-4 px-5 bg-grey-lightest flex shadow justify-between items-center">
+        <div className="p-3 bg-grey-light rounded hover:bg-grey select-none cursor-pointer">
+          Ajna | Feasibility Estimator
+        </div>
+      </div>
+      <div className="absolute opacity-75 text-black px-5 py-2 font-mono text-xs flex flex-row-reverse bg-grey w-full">
+        Using NASA WorldWind Data
       </div>
     </div>
   )
