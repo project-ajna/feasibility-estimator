ffdd71d93f8384938f8fb8551eca9ecf6797d103
Nikhil Mohan <nikhil@qucentis.com>
2018-08-03 23:08:38 +0530

    added calculations for area and cost

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import os
+import math
 import json
 import tempfile
 
@@ -27,12 +28,12 @@ def forest_cover_img(img):
 
     forest = cv2.countNonZero(mask)
 
-    percentage = (forest/total)*100
-
-    return percentage
+    return forest/total
 
 
 app = Flask(__name__)
+
+R = 6371
 
 @app.route('/')
 def hello_world():
@@ -43,20 +44,33 @@ def get_forest_cover():
     width = int(request.args.get('width', '256'))
     height = int(request.args.get('height', '256'))
     bbox = request.args.get('bbox', '')
+    coords = list(map(float, bbox.split(",")))
+
+    lat1, lon1 = coords[0] * math.pi / 180, coords[1] * math.pi / 180
+    lat2, lon2 = coords[2] * math.pi / 180, coords[3] * math.pi / 180
+
+    area = (math.pi/180) * R * R * abs(math.sin(lat1) - math.sin(lat2)) * abs(lon1 - lon2)
+
+    cost = area * 100
     
     url = "https://worldwind25.arc.nasa.gov/wms?service=WMS&request=GetMap&version=1.3.0&transparent=TRUE&layers=BlueMarble-200405,esat&styles=&format=image/jpeg&width=%d&height=%d&crs=EPSG:4326&bbox=%s" % (width, height, bbox)
 
     if MOCK:
-        cover = float("%.2f" % forest_cover("img-orig.jpg"))
+        cover = forest_cover("img-orig.jpg")
     else:
         with tempfile.NamedTemporaryFile(dir='/tmp') as tmpfile:
             f = tmpfile.name
 
         r = requests.get(url, allow_redirects=True)
         open(f, 'wb').write(r.content)
-        cover = float("%.2f" % forest_cover(f))
+        cover = forest_cover(f)
+
+    area, cost = int(area * cover), int(cover * cost)
+    cover = float("%.2f" % (100 * cover))
     
-    result = {"cover": cover, "url": url}
+    result = {"cover": cover, "area": area, "cost": cost, "url": url}
+
+    print(result)
 
     if not MOCK: os.unlink(f)
 
